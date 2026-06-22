3facc3868ceed9ac2754a9c52dd4b493dbe1c429
B0SE <~@b0se.dev>
2026-06-22 15:33:31 +0200

    Fix(ICMP): timeouts and domain based pings

--- src/icmp.rs
+++ src/icmp.rs
@@ -13,8 +13,16 @@ pub async fn ping_icmp(config: &PingArgs) -> Result<Vec<Option<Duration>>, std::
         .ok_or_else(|| std::io::Error::new(std::io::ErrorKind::NotFound, "Could not resolve host"))?
         .ip();
 
-    let client = Client::new(&Config::default())?;
-    let mut pinger = client.pinger(addr, PingIdentifier(111)).await;
+    let client_config = match addr {
+        std::net::IpAddr::V4(_) => Config::default(),
+        std::net::IpAddr::V6(_) => Config::builder().kind(surge_ping::ICMP::V6).build(),
+    };
+
+    let client = Client::new(&client_config)?;
+    let mut pinger = client
+        .pinger(addr, PingIdentifier(std::process::id() as u16))
+        .await;
+    pinger.timeout(Duration::from_secs(5));
     let mut sequence = 0u16;
 
     loop {
@@ -22,7 +30,7 @@ pub async fn ping_icmp(config: &PingArgs) -> Result<Vec<Option<Duration>>, std::
             _ = async {
                 let payload = [0u8; 56];
 
-                match timeout(Duration::from_secs(2), pinger.ping(PingSequence(sequence), &payload)).await {
+                match timeout(Duration::from_secs(5), pinger.ping(PingSequence(sequence), &payload)).await {
                     Ok(Ok((_, elapsed))) => {
                         durations.push(Some(elapsed));
                         print_response(&PingResponse {
